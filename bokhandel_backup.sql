3866e93523830e6545fa162cc3196db1607fc159
AleexanderrJ <alexander.johansson@student.ksgyf.se>
2026-03-11 21:03:32 +0100

    Adderat bokhandel_backup.sql

---
+++ bokhandel_backup.sql
@@ -0,0 +1,193 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: inlamning1
+-- ------------------------------------------------------
+-- Server version	9.5.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+
+--
+-- Table structure for table `bestallningar`
+--
+
+DROP TABLE IF EXISTS `bestallningar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bestallningar` (
+  `Ordernummer` int NOT NULL AUTO_INCREMENT,
+  `KundID` int NOT NULL,
+  `Totalbelopp` decimal(10,2) NOT NULL,
+  `Datum` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Ordernummer`),
+  KEY `KundID` (`KundID`),
+  CONSTRAINT `bestallningar_ibfk_1` FOREIGN KEY (`KundID`) REFERENCES `kunder` (`KundID`),
+  CONSTRAINT `bestallningar_chk_1` CHECK ((`Totalbelopp` > 0))
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bestallningar`
+--
+
+LOCK TABLES `bestallningar` WRITE;
+/*!40000 ALTER TABLE `bestallningar` DISABLE KEYS */;
+INSERT INTO `bestallningar` VALUES (1,1,79.99,'2025-12-03 07:33:48'),(2,2,99.99,'2025-12-03 07:33:48'),(3,3,199.98,'2025-12-03 07:33:48'),(4,1,239.98,'2026-03-11 14:36:34'),(5,1,99.99,'2026-03-11 14:40:35');
+/*!40000 ALTER TABLE `bestallningar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bocker`
+--
+
+DROP TABLE IF EXISTS `bocker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bocker` (
+  `ISBN` bigint NOT NULL,
+  `Forfattare` varchar(100) NOT NULL,
+  `Genre` varchar(50) NOT NULL,
+  `Titel` varchar(100) NOT NULL,
+  `Pris` decimal(10,2) NOT NULL,
+  `Lagerstatus` int NOT NULL,
+  PRIMARY KEY (`ISBN`),
+  CONSTRAINT `bocker_chk_1` CHECK ((`Pris` > 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bocker`
+--
+
+LOCK TABLES `bocker` WRITE;
+/*!40000 ALTER TABLE `bocker` DISABLE KEYS */;
+INSERT INTO `bocker` VALUES (9324234311,'Brian Herbet','Sci-fi','Dune: House Atreides',119.99,5),(9634982340,'Ravrek al-Dahim','Sci-fi','Chronicles of the Spice Horizon',99.99,3),(9834032234,'Frank Herbert','Sci-fi','Dune',79.99,10);
+/*!40000 ALTER TABLE `bocker` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kunder`
+--
+
+DROP TABLE IF EXISTS `kunder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kunder` (
+  `KundID` int NOT NULL AUTO_INCREMENT,
+  `Namn` varchar(100) NOT NULL,
+  `Epost` varchar(255) NOT NULL,
+  `Telefon` varchar(30) NOT NULL,
+  `Adress` varchar(100) NOT NULL,
+  PRIMARY KEY (`KundID`),
+  UNIQUE KEY `Epost` (`Epost`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kunder`
+--
+
+LOCK TABLES `kunder` WRITE;
+/*!40000 ALTER TABLE `kunder` DISABLE KEYS */;
+INSERT INTO `kunder` VALUES (1,'Paul Atreides','paul.atreides@dune.com','123','Arrakis'),(2,'Duncan Idaho','duncan.idaho@dune.com','456','Arrakis'),(3,'Glossu Rabban','glossu.rabban@dune.com','789','Arrakis'),(4,'Test Deletion','test.deletion@dune.com','789','Arakkis');
+/*!40000 ALTER TABLE `kunder` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `logga_ny_kund` AFTER INSERT ON `kunder` FOR EACH ROW BEGIN
+   INSERT INTO Kundlogg (KundID)
+   VALUES (NEW.KundID);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `orderrader`
+--
+
+DROP TABLE IF EXISTS `orderrader`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderrader` (
+  `OrderradID` int NOT NULL AUTO_INCREMENT,
+  `ISBN` bigint NOT NULL,
+  `Ordernummer` int NOT NULL,
+  `Antal` int NOT NULL,
+  PRIMARY KEY (`OrderradID`),
+  KEY `Ordernummer` (`Ordernummer`),
+  KEY `ISBN` (`ISBN`),
+  CONSTRAINT `orderrader_ibfk_1` FOREIGN KEY (`Ordernummer`) REFERENCES `bestallningar` (`Ordernummer`),
+  CONSTRAINT `orderrader_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `bocker` (`ISBN`),
+  CONSTRAINT `orderrader_chk_1` CHECK ((`Antal` > 0))
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderrader`
+--
+
+LOCK TABLES `orderrader` WRITE;
+/*!40000 ALTER TABLE `orderrader` DISABLE KEYS */;
+INSERT INTO `orderrader` VALUES (1,9834032234,1,1),(2,9634982340,2,1),(3,9324234311,3,2),(4,9324234311,4,2),(5,9634982340,5,1);
+/*!40000 ALTER TABLE `orderrader` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `uppdatera_lagersaldo` AFTER INSERT ON `orderrader` FOR EACH ROW BEGIN
+    UPDATE Bocker
+    SET Lagerstatus = Lagerstatus - NEW.Antal
+    WHERE ISBN = NEW.ISBN;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'inlamning1'
+--
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-11 20:11:47
